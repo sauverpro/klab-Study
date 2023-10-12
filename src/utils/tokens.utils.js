a17c0835a47e5cfb943dad8898c9896cf9042ec9
sauverpro <unsauvechriss@gmail.com>
2023-10-12 12:06:59 +0200

    file to create token for users

---
+++ src/utils/tokens.utils.js
@@ -0,0 +1,8 @@
+import Jwt from "jsonwebtoken";
+
+export const generateToken = (data) => {
+  let token = Jwt.sign(data, process.env.JWT_SECRETE, {
+    expiresIn: process.env.JWT_EXP,
+  });
+  return token
+};
